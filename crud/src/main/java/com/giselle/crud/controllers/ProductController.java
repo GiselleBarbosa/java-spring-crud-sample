4c0fe21112e662ef37ad93016a9e919c622eae5a
GiselleBarbosa <g88barbosa@gmail.com>
2024-03-06 23:53:55 -0300

    criado metodo POST

--- crud/src/main/java/com/giselle/crud/controllers/ProductController.java
+++ crud/src/main/java/com/giselle/crud/controllers/ProductController.java
@@ -1,11 +1,12 @@
 package com.giselle.crud.controllers;
 
+import com.giselle.crud.domains.product.Product;
 import com.giselle.crud.domains.product.ProductRepository;
+import com.giselle.crud.domains.product.RequestProductDTO;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/product")
@@ -17,7 +18,13 @@ public class ProductController {
     @GetMapping
     public ResponseEntity getAllProducts() {
         var allProducts = repository.findAll();
-
         return ResponseEntity.ok(allProducts);
+    }
+
+    @PostMapping
+    public ResponseEntity createProduct(@RequestBody @Valid RequestProductDTO data) {
+        Product newProduct = new Product(data);
+        repository.save(newProduct);
+        return ResponseEntity.ok().build();
     }
 }
