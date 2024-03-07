c9067c923179127d95427d067e80dcc43faf7c2a
GiselleBarbosa <g88barbosa@gmail.com>
2024-03-06 22:45:04 -0300

    adicionada migration v1

---
+++ crud/src/main/java/com/giselle/crud/domains/product/Product.java
@@ -0,0 +1,2 @@
+package com.giselle.crud.domains.product;public class Product {
+}
---
+++ crud/src/main/resources/db/migration/V1__create-table-products.sql
@@ -0,0 +1,5 @@
+CREATE TABLE product(
+id TEXT NOT NULL PRIMARY KEY UNIQUE,
+name TEXT NOT NULL,
+price_in_cents INT NOT NULL
+);
